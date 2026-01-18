19c49986779557f6865769a45070ced90a580c01
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2026-01-18 14:10:16 +0100

    Use Magisk and KSU from latest Orangefox source
    Use Magisk and KSU from latest Orangefox source

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,9 +49,6 @@ export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
 # Set default Keymaster Version
 export OF_DEFAULT_KEYMASTER_VERSION=4.1
 
-# Magisk
-export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v28.1.zip
-
 # Add some extras
 export FOX_USE_ZIP_BINARY=1
 export FOX_USE_TAR_BINARY=1
